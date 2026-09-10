d729358e121270b6024e16aa4068e7b07e2ea94a
SHUBHRANIL ROY <shubhranil96@gmail.com>
2026-09-10 23:00:23 +0530

    Add query to analyze profit/loss by manufacturer

--- Pharmacy Analytics.sql
+++ Pharmacy Analytics.sql
@@ -7,3 +7,13 @@ order by (total_sales-cogs) DESC
 limit 3)
 
 SELECT * from profit_of_goods
+
+
+ /*part 2 */
+SELECT manufacturer,
+count(drug),
+sum(cogs-total_sales)
+FROM pharmacy_sales
+where (total_sales-cogs)<0
+group by manufacturer
+order by sum desc;
