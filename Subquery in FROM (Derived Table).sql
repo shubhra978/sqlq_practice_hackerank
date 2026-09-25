40db79ca05860a07f160756d75aed2359784dcd0
SHUBHRANIL ROY <shubhranil96@gmail.com>
2026-09-25 21:38:24 +0530

    Refactor SQL query to use order_detail CTE

--- Subquery in FROM (Derived Table).sql
+++ Subquery in FROM (Derived Table).sql
@@ -1,14 +1,11 @@
-with order_CTE as (
-  select customer_id,
-  count(order_id) as order_count
-  from orders group by customer_id)
-
+with order_detail as (select customer_id,count(order_id) as order_count from orders group by customer_id)
 select name,
 membership,
 order_count,
-case when order_count > (select avg(order_count)from order_CTE) then 'Yes' else 'No' end as above_average
-from order_CTE
-inner join
-customers on order_CTE.customer_id  = customers.customer_id
-group by customers.customer_id
+case when order_count > avg(order_count) over() then 'Yes' else 'No' end as above_average 
+from customers
+inner join order_detail
+on customers.customer_id = order_detail.customer_id
+group by order_detail.customer_id
 order by order_count desc, name
+
